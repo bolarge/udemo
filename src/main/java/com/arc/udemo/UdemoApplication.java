846c5152c7673dc88bf5bc7eee10fd89007d9ba9
Bolaji Salau <bolaji.salau@gmail.com>
2021-03-19 16:05:24 +0100

    Initial Setup
    Signed-off-by: Bolaji Salau <bolaji.salau@gmail.com>

---
+++ src/main/java/com/arc/udemo/UdemoApplication.java
@@ -0,0 +1,13 @@
+package com.arc.udemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemoApplication.class, args);
+	}
+
+}
